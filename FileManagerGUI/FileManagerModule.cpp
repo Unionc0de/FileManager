7b721a8bc1cc01418b212829b09e9a0acf485889
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2025-01-21 18:30:07 +0300

    Update FileManagerModule.cpp

--- FileManagerGUI/FileManagerModule.cpp
+++ FileManagerGUI/FileManagerModule.cpp
@@ -2,40 +2,34 @@
 #include <windows.h>
 #include <string>
 #include <list>
+#include <msclr/marshal_cppstd.h>
 
 using namespace std;
 
 list<string> searchFile(string path) {
-    list<string> result;  // Список для хранения найденных файлов и папок
-    setlocale(LC_ALL, "rus");
+	list<string> result;
+	setlocale(LC_ALL, "rus");
 
-    // Добавляем шаблон "\*" для поиска всех файлов/папок в директории
-    path += "\\*";
+	path += "\\*";
 
-    // Структура для хранения информации о файле или папке
-    WIN32_FIND_DATAA findFileData;
+	WIN32_FIND_DATAA findFileData;
 
-    // Функция FindFirstFileA возвращает дескриптор первого файла в папке
-    HANDLE hFind = FindFirstFileA(path.c_str(), &findFileData);
+	HANDLE hFind = FindFirstFileA(path.c_str(), &findFileData);
 
-    // Проверяем, удалось ли открыть папку
-    if (hFind == INVALID_HANDLE_VALUE) {
-        cerr << "Ошибка при открытии директории! Проверьте путь." << endl;
-    }
-    else {
-        // Цикл для перебора всех файлов и папок
-        do {
-            string fileName = findFileData.cFileName;
+	if (hFind == INVALID_HANDLE_VALUE) {
+		cerr << "Path error" << endl;
+	}
+	else {
+		do {
+			string fileName = findFileData.cFileName;
 
-            // Исключаем системные директории "." и ".."
-            if (fileName != "." && fileName != "..") {
-                result.push_back(fileName);  // Добавляем имя файла/папки в список
-            }
-        } while (FindNextFileA(hFind, &findFileData) != 0); // Переходим к следующему файлу/папке
+			if (fileName != "." && fileName != "..") {
+				result.push_back(fileName);
+			}
+		} while (FindNextFileA(hFind, &findFileData) != 0);
 
-        // Закрываем дескриптор, чтобы освободить ресурсы
-        FindClose(hFind);
-    }
+		FindClose(hFind);
+	}
 
-    return result;  // Возвращаем список файлов/папок
+	return result;
 }
